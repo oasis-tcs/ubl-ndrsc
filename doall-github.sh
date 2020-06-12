060ec4d36b4dbf57de05b0a25787f66871fca31b
gkholman <gkenholman+github@gmail.com>
2020-06-12 12:30:40 -0400

    Remove debug

--- doall-github.sh
+++ doall-github.sh
@@ -2,5 +2,4 @@
 #
 # This is the invocation that happens in the GitHub action ... it must be bash
 #
-echo bash doall.sh "$1" "$2" "$3" "$4" "$5" "$6" "$7"
 bash doall.sh "$1" "$2" "$3" "$4" "$5" "$6" "$7"
