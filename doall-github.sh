5053bf88112ef0d078de803ebd12405f5add5de7
gkholman <gkenholman+github@gmail.com>
2020-06-12 11:48:20 -0400

    Diagnose invocations

--- doall-github.sh
+++ doall-github.sh
@@ -2,4 +2,5 @@
 #
 # This is the invocation that happens in the GitHub action ... it must be bash
 #
+echo bash doall.sh "$1" "$2" "$3" "$4" "$5" "$6" "$7"
 bash doall.sh "$1" "$2" "$3" "$4" "$5" "$6" "$7"
