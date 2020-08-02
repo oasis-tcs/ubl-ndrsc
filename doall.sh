c8c1f8073c9e5a5d25fbdf0426eaabacbe157305
gkholman <gkenholman+github@gmail.com>
2020-08-02 11:04:52 -0400

    Debug multipart boundary

--- doall.sh
+++ doall.sh
@@ -17,21 +17,21 @@ touch $1/ndr.console.$3.txt
 echo UBL Governance 1.1...                               | tee -a $1/ndr.console.$3.txt
 bash packageGovernance.sh $1 cnd01 $3 $4 $5 $6       | tee -a $1/ndr.console.$3.txt
 UBLGovReturn=${PIPESTATUS[0]}
-echo UBL 2.1 JSON 2.0...                                | tee -a $1/ndr.console.$3.txt
-bash packageUBLJSON.sh $1 2.1 2.0 cnd01 $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
-UBL21Return=${PIPESTATUS[0]}
-echo UBL 2.2 JSON 1.0...                                | tee -a $1/ndr.console.$3.txt
-bash packageUBLJSON.sh $1 2.2 1.0 cnd01 $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
-UBL22Return=${PIPESTATUS[0]}
-echo UBL 2.3 JSON 1.0...                                | tee -a $1/ndr.console.$3.txt
-bash packageUBLJSON.sh $1 2.3 1.0 cnd01 $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
-UBL23Return=${PIPESTATUS[0]}
-echo UBL NDR 3.1...                                     | tee -a $1/ndr.console.$3.txt
-bash packageUBLNDR.sh $1 cnd01 $3 $4 $5 $6          | tee -a $1/ndr.console.$3.txt
-UBLNDRReturn=${PIPESTATUS[0]}
-echo BDNDR 1.1...                                       | tee -a $1/ndr.console.$3.txt
-bash packageBDNDR11.sh $1 csd03 $3 $4 $5 $6         | tee -a $1/ndr.console.$3.txt
-BDNDRReturn=${PIPESTATUS[0]}
+#echo UBL 2.1 JSON 2.0...                                | tee -a $1/ndr.console.$3.txt
+#bash packageUBLJSON.sh $1 2.1 2.0 cnd01 $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
+#UBL21Return=${PIPESTATUS[0]}
+#echo UBL 2.2 JSON 1.0...                                | tee -a $1/ndr.console.$3.txt
+#bash packageUBLJSON.sh $1 2.2 1.0 cnd01 $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
+#UBL22Return=${PIPESTATUS[0]}
+#echo UBL 2.3 JSON 1.0...                                | tee -a $1/ndr.console.$3.txt
+#bash packageUBLJSON.sh $1 2.3 1.0 cnd01 $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
+#UBL23Return=${PIPESTATUS[0]}
+#echo UBL NDR 3.1...                                     | tee -a $1/ndr.console.$3.txt
+#bash packageUBLNDR.sh $1 cnd01 $3 $4 $5 $6          | tee -a $1/ndr.console.$3.txt
+#UBLNDRReturn=${PIPESTATUS[0]}
+#echo BDNDR 1.1...                                       | tee -a $1/ndr.console.$3.txt
+#bash packageBDNDR11.sh $1 csd03 $3 $4 $5 $6         | tee -a $1/ndr.console.$3.txt
+#BDNDRReturn=${PIPESTATUS[0]}
 
 mv $1/ndr.console.$3.txt $1/ndr-UBL-$2-$3/archive-only-not-in-final-distribution/
 echo Governance:$UBLGovReturn UBL21:$UBL21Return UBL22:$UBL22Return UBL23:$UBL23Return UBLNDR:$UBLNDRReturn BDNDR:$BDNDRReturn  >$1/ndr-UBL-$2-$3/archive-only-not-in-final-distribution/ndr.exitcodes.$3.txt
