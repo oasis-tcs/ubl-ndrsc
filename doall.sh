0f4c3a948817a6a2b9ca02f0c2ec5d7bfe7ea699
gkholman <gkenholman+github@gmail.com>
2021-09-07 13:43:41 -0400

    Prepare first voting documents of final drafts; no longer working drafts

--- doall.sh
+++ doall.sh
@@ -23,19 +23,19 @@ popd
 #bash packageGovernance.sh $1 cn01 $3 $4 $5 $6       | tee -a $1/ndr.console.$3.txt
 #UBLGovReturn=${PIPESTATUS[0]}
 echo UBL 2.1 JSON 2.0...                                | tee -a $1/ndr.console.$3.txt
-bash packageUBLJSON.sh $1 2.1 2.0 cnd02wd01 $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
+bash packageUBLJSON.sh $1 2.1 2.0 cnd02     $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
 UBL21Return=${PIPESTATUS[0]}
 echo UBL 2.2 JSON 1.0...                                | tee -a $1/ndr.console.$3.txt
-bash packageUBLJSON.sh $1 2.2 1.0 cnd02wd01 $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
+bash packageUBLJSON.sh $1 2.2 1.0 cnd02     $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
 UBL22Return=${PIPESTATUS[0]}
 echo UBL 2.3 JSON 1.0...                                | tee -a $1/ndr.console.$3.txt
-bash packageUBLJSON.sh $1 2.3 1.0 cnd02wd01 $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
+bash packageUBLJSON.sh $1 2.3 1.0 cnd02     $3 $4 $5 $6 | tee -a $1/ndr.console.$3.txt
 UBL23Return=${PIPESTATUS[0]}
 #echo UBL NDR 3.1...                                     | tee -a $1/ndr.console.$3.txt
 #bash packageUBLNDR.sh $1 cn01 $3 $4 $5 $6          | tee -a $1/ndr.console.$3.txt
 #UBLNDRReturn=${PIPESTATUS[0]}
 echo BDNDR 1.1...                                       | tee -a $1/ndr.console.$3.txt
-bash packageBDNDR11.sh $1 csd04wd01 $3 $4 $5 $6         | tee -a $1/ndr.console.$3.txt
+bash packageBDNDR11.sh $1 csd04     $3 $4 $5 $6         | tee -a $1/ndr.console.$3.txt
 BDNDRReturn=${PIPESTATUS[0]}
 
 mv $1/ndr.console.$3.txt $1/ndr-UBL-$2-$3/archive-only-not-in-final-distribution/
